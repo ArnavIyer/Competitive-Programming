b26c7418ddf90aa78999f3448f696e42941fd1fd
ArnavIyer <40965890+ArnavIyer@users.noreply.github.com>
2019-04-08 13:13:29 -0500

    Update grid.cpp

--- Kattis/grid.cpp
+++ Kattis/grid.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <list>
-#include <set>
+#include <map>
 using namespace std;
 
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
 
 int main() {
-	//set<int> used;
+	map<int,int> ans;
 	int n,m; cin >> n >> m;
 	int num = n*m;
 	int adj[num][num] = {0};
-	//cout << adj[0][2] << endl << endl;
 	for (int i = 0; i<n*m; i++) {
 			adj[0][i] = 0;
-	}	
+	}
+	cin.ignore();
+	string in;
 	for (int i = 0; i<n; i++) {	
+		getline(cin, in);
 		for (int j = 0; j<m; j++) {
-			//if (i == 4 && j == 2) cout << "D ' " << endl;
-			//cout << i << " " << j << endl;
-			int x; cin >> x;	
+			int x = in[j] - '0';	
 			for (int z=0;z<4;z++) {
 				if (dx[z]*x+i < 0 || dx[z]*x+i >= n || dy[z]*x +j<0 || dy[z]*x +j>=m) {continue;}
 				adj[i*m+j][(i+dx[z]*x)*m+j+dy[z]*x] = 1;//i=4,j=1,z=1
 			}
 		}
-	}
-	
-	for (int i = 0; i<n*m; i++) {
-		for (int j = 0 ; j<n*m; j++) {
-			cout << adj[i][j];
-		}
-		cout << endl;
 	}
 	int s = 0;
 	bool visited[n*m] = {false};
@@ -42,23 +35,27 @@ int main() {
 	int count = 0;
 	while(!queue.empty()) {
 		s = queue.front();
-		//used.insert(s);
-		cout << s << " ";
 		queue.pop_front();
-		count++;
 		for (int i = 0; i<n*m; i++) {
 			if (adj[s][i]!=1) continue;
 			if (!visited[i]) {
 				if (i == n*m-1) {
 					done = true;
+					ans[n*m-1] = s;
 					break;
 				}
 				visited[i] = true;
 				queue.push_back(i);
+				ans[i] = s;
 			}
 		}
 		if (done) break;
 	}
-	if (done) cout << "end" << " "<< count<< endl;
-	//cout << endl << used.size() << endl;
+	int curr = n*m-1;
+	while (curr != 0) {
+		curr = ans[curr];
+		count++;
+	}
+	if (done) cout << count<< endl;
+	else cout << -1 << endl;
 }
