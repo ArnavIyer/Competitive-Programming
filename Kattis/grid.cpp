20a2645f67b9f10f2c38e6896defcb9b92f81adb
ArnavIyer <40965890+ArnavIyer@users.noreply.github.com>
2019-04-08 20:41:56 -0500

    Update grid.cpp

--- Kattis/grid.cpp
+++ Kattis/grid.cpp
@@ -8,12 +8,12 @@ int dy[4] = {0,0,1,-1};
 
 int main() {
 	map<int,int> ans;
+
 	int n,m; cin >> n >> m;
-	int num = n*m;
-	int adj[num][num] = {0};
-	for (int i = 0; i<n*m; i++) {
-			adj[0][i] = 0;
-	}
+	int V = n*m;
+	list<int> *adj;
+	adj = new list<int>[V];
+		
 	cin.ignore();
 	string in;
 	for (int i = 0; i<n; i++) {	
@@ -22,7 +22,7 @@ int main() {
 			int x = in[j] - '0';	
 			for (int z=0;z<4;z++) {
 				if (dx[z]*x+i < 0 || dx[z]*x+i >= n || dy[z]*x +j<0 || dy[z]*x +j>=m) {continue;}
-				adj[i*m+j][(i+dx[z]*x)*m+j+dy[z]*x] = 1;//i=4,j=1,z=1
+				adj[i*m+j].push_back((i+dx[z]*x)*m+j+dy[z]*x);
 			}
 		}
 	}
@@ -33,20 +33,20 @@ int main() {
 	queue.push_back(s);
 	bool done = false;
 	int count = 0;
+	list<int>::iterator i;
 	while(!queue.empty()) {
 		s = queue.front();
 		queue.pop_front();
-		for (int i = 0; i<n*m; i++) {
-			if (adj[s][i]!=1) continue;
-			if (!visited[i]) {
-				if (i == n*m-1) {
+		for (i = adj[s].begin(); i!=adj[s].end(); i++) {
+			if (!visited[*i]) {
+				if (*i == n*m-1) {
 					done = true;
 					ans[n*m-1] = s;
 					break;
 				}
-				visited[i] = true;
-				queue.push_back(i);
-				ans[i] = s;
+				visited[*i] = true;
+				queue.push_back(*i);
+				ans[*i] = s;
 			}
 		}
 		if (done) break;
